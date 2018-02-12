721a4e5c33913c5ac0946cdf2b793c6eca1744f5
Javier Martinez <fjmartinez@disitec.es>
2018-02-12 18:08:23 +0100

    ep3 take1

--- tests/App.spec.js
+++ tests/App.spec.js
@@ -10,3 +10,15 @@ test('default data is correct', () => {
   const wrapper = mount(App);
   expect(wrapper.vm.msg).toBe('Hello World!');
 });
+
+test('msg data is displayed', () => {
+  const wrapper = mount(App);
+  expect(wrapper.text()).toContain('Hello World!');
+});
+
+test('msg changes when button is clicked', () => {
+  const wrapper = mount(App);
+  const button = wrapper.find('button');
+  button.trigger('click');
+  expect(wrapper.html()).toContain('Updated text');
+});
